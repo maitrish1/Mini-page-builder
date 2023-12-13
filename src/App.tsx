b788bcf7c6d5ab1ac4fe30d1dbf222b28f1738b0
Maitrish Mukherjee <maitrish03@gmail.com>
2023-12-14 00:31:09 +0530

    elements stick where dropped

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,41 @@ import Board from "./components/Board";
 import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [boardElements, setBoardElements] = useState<string[]>([]);
+  const [boardElements, setBoardElements] = useState<{
+    id: string;
+    type: string;
+    top: number;
+    left: number;
+  }[]>([]);
 
-  const handleDrop = (elementType: string) => {
-    setBoardElements((prevElements) => [...prevElements, elementType]);
+  const handleDrop = (elementType: string, top: number, left: number) => {
+    const newElement = {
+      id: `element-${boardElements.length}`,
+      type: elementType,
+      top,
+      left,
+    };
+    console.log(newElement)
+    setBoardElements((prevElements) => [...prevElements, newElement]);
   };
 
-  const handleDragStart = (e: React.DragEvent, elementType: string) => {
-    e.dataTransfer.setData("text", elementType);
+  console.log(boardElements)
+  const handleMove = (id: string, top: number, left: number) => {
+    setBoardElements((prevElements) =>
+      prevElements.map((element) =>
+        element.id === id ? { ...element, top, left } : element
+      )
+    );
+  };
+
+  const handleDragStart = (e: React.DragEvent, id: string) => {
+    e.dataTransfer.setData("text", id);
   };
 
   return (
     <div className="h-screen w-full flex">
-      <div className="w-3/4 h-full">
-        <Board elements={boardElements} onDrop={handleDrop} />
+      <div className="w-3/4 h-full bg-slate-200">
+        <Board elements={boardElements} onDrop={handleDrop} onMove={handleMove} />
       </div>
       <div className="w-1/4 h-full">
         <Sidebar onDragStart={handleDragStart} />
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,34 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface BoardProps {
-  elements: string[];
-  onDrop: (elementType: string) => void;
+  elements: { id: string; type: string; top: number; left: number }[];
+  onDrop: (elementType: string, top: number, left: number) => void;
+  onMove: (id: string, top: number, left: number) => void;
 }
 
-const Board: React.FC<BoardProps> = ({ elements, onDrop }) => {
-  const handleDrop = (e: React.DragEvent) => {
-    e.preventDefault();
-    const elementType = e.dataTransfer.getData("text");
-    onDrop(elementType);
+
+const Board: React.FC<BoardProps> = ({ elements, onDrop, onMove }) => {
+    const [draggedElement, setDraggedElement] = useState<string | null>(null);
+  
+    const handleDrop = (e: React.DragEvent) => {
+      e.preventDefault();
+        const elementType = e.dataTransfer.getData("text");
+        const { top, left } = e.currentTarget.getBoundingClientRect();
+        console.log('Dropped element:', elementType, 'Coordinates:', e.clientY - top, e.clientX - left);
+        onDrop(elementType, e.clientY - top, e.clientX - left);
+        setDraggedElement(null);
+      
+    };
+  
+    const handleDragStart = (e: React.DragEvent, id: string) => {
+      console.log('handleDragStart called');
+      setDraggedElement(id);
+      e.dataTransfer.setData("text", id);
+    };
+  
+    const handleMouseMove = (e: React.MouseEvent) => {
+      if (draggedElement) {
+        const { top, left } = e.currentTarget.getBoundingClientRect();
+        console.log('Moving element:', draggedElement, 'Coordinates:', e.clientY - top, e.clientX - left);
+        onMove(draggedElement, e.clientY - top, e.clientX - left);
+      }
+    };
+    
+    return (
+      <div
+        className="w-full relative h-full"
+        onDrop={handleDrop}
+        onDragOver={(e) => e.preventDefault()}
+        onMouseMove={handleMouseMove}
+      >
+        {elements.map((element) => (
+          <div
+            key={element.id}
+            className="absolute"
+            style={{ top: element.top, left: element.left }}
+            draggable
+            onDragStart={(e) => handleDragStart(e, element.id)}
+          >
+            {element.top}, {element.left}
+            {element.type}
+          </div>
+        ))}
+      </div>
+    );
   };
-
-  const handleDragOver = (e: React.DragEvent) => {
-    e.preventDefault();
-  };
-
-  return (
-    <div
-      className="w-full h-full"
-      onDrop={handleDrop}
-      onDragOver={handleDragOver}
-    >
-      {elements.map((element, index) => (
-        <div key={index} className="board-element">
-          {element}
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Board;
+  
+  export default Board;
+  
