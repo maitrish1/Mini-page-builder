a2edbb96afd2a60c2b72e4d703e049e8fa4f7124
Maitrish Mukherjee <maitrish03@gmail.com>
2023-12-15 19:18:23 +0530

    finished with localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,65 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import Board from "./components/Board";
 import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [boardElements, setBoardElements] = useState<{
+  const storedElements = localStorage.getItem("boardElements");
+  const initialBoardElements: {
     id: string;
     type: string;
     top: number;
     left: number;
-  }[]>([]);
+    text: string;
+    fontSize: number;
+    fontWeight: string;
+  }[] = storedElements ? JSON.parse(storedElements) : [];
 
-  const handleDrop = (elementType: string, top: number, left: number) => {
+  const [boardElements, setBoardElements] = useState(initialBoardElements);
+
+  useEffect(() => {
+    // Update local storage whenever boardElements changes
+    localStorage.setItem("boardElements", JSON.stringify(boardElements));
+  }, [boardElements]);
+
+  const handleDrop = (
+    elementType: string,
+    top: number,
+    left: number,
+    text: string,
+    fontSize: number,
+    fontWeight: string
+  ) => {
     const newElement = {
       id: `element-${boardElements.length}`,
       type: elementType,
       top,
       left,
+      text,
+      fontSize,
+      fontWeight,
     };
     setBoardElements((prevElements) => [...prevElements, newElement]);
   };
 
   const handleMove = (id: string, top: number, left: number) => {
-    console.log('inside handlemove app.tsx')
     setBoardElements((prevElements) =>
       prevElements.map((element) =>
         element.id === id ? { ...element, top, left } : element
+      )
+    );
+  };
+
+  const handleEditMove = (
+    id: string,
+    top: number,
+    left: number,
+    text: string,
+    fontSize: number,
+    fontWeight: string
+  ) => {
+    setBoardElements((prevElements) =>
+      prevElements.map((element) =>
+        element.id === id ? { ...element, top, left, text, fontSize, fontWeight } : element
       )
     );
   };
@@ -36,7 +71,12 @@ function App() {
   return (
     <div className="h-screen w-full flex">
       <div className="w-3/4 h-full bg-slate-200">
-        <Board elements={boardElements} onDrop={handleDrop} onMove={handleMove} />
+        <Board
+          elements={boardElements}
+          onEditMove={handleEditMove}
+          onDrop={handleDrop}
+          onMove={handleMove}
+        />
       </div>
       <div className="w-1/4 h-full">
         <Sidebar onDragStart={handleDragStart} />
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,45 +1,189 @@
-import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import * as React from "react";
+import { useState } from "react";
 
 interface BoardProps {
-  elements: { id: string; type: string; top: number; left: number }[];
-  onDrop: (elementType: string, top: number, left: number) => void;
+  elements: {
+    id: string;
+    type: string;
+    top: number;
+    left: number;
+    text: string;
+    fontSize: number;
+    fontWeight: string;
+  }[];
+  onDrop: (
+    elementType: string,
+    top: number,
+    left: number,
+    text: string,
+    fontSize: number,
+    fontWeight: string
+  ) => void;
   onMove: (id: string, top: number, left: number) => void;
+  onEditMove: (
+    id: string,
+    top: number,
+    left: number,
+    text: string,
+    fontSize: number,
+    fontWeight: string
+  ) => void;
 }
 
+const Board: React.FC<BoardProps> = ({
+  elements,
+  onDrop,
+  onMove,
+  onEditMove,
+}) => {
+  const [draggedElement, setDraggedElement] = useState<string | null>(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [currentElement, setCurrentElement] = useState<{
+    type: string;
+    id: string;
+  } | null>(null);
+  const [elementData, setElementData] = useState<{
+    text: string;
+    x: number;
+    y: number;
+    fontSize: number;
+    fontWeight: string;
+  }>({ text: "", x: 0, y: 0, fontSize: 16, fontWeight: "normal" });
+  const [editingElementId, setEditingElementId] = useState<string | null>(null);
 
-const Board: React.FC<BoardProps> = ({ elements, onDrop, onMove }) => {
-    const [draggedElement, setDraggedElement] = useState<string | null>(null);
-  
-    const handleDrop = (e: React.DragEvent) => {
-        e.preventDefault();
-        const { top, left } = e.currentTarget.getBoundingClientRect();
-        const elementType = e.dataTransfer.getData("text");
-        const id = e.dataTransfer.getData("id");
-      
-        if (id) {
-          onMove(id, e.clientY - top, e.clientX - left);
-        } else {
-          onDrop(elementType, e.clientY - top, e.clientX - left);
-        }
-        setDraggedElement(null);
-      };
-  
-      const handleDragStart = (e: React.DragEvent, id: string) => {
-        setDraggedElement(id);
-        e.dataTransfer.setData("text", "existingElement");
-        e.dataTransfer.setData("id", id);
-      };
-  
-    const handleMouseMove = (e: React.MouseEvent) => {
-      if (draggedElement) {
-        console.log('inside if draggeedlement')
-        const { top, left } = e.currentTarget.getBoundingClientRect();
-        console.log('Moving element:', draggedElement, 'Coordinates:', e.clientY - top, e.clientX - left);
-        onMove(draggedElement, e.clientY - top, e.clientX - left);
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    const { top, left } = e.currentTarget.getBoundingClientRect();
+    const elementType = e.dataTransfer.getData("text");
+    const id = e.dataTransfer.getData("id");
+
+    if (id) {
+      onMove(id, e.clientY - top, e.clientX - left);
+    } else {
+      setCurrentElement({
+        type: elementType,
+        id: `element-${elements.length}`,
+      });
+      setModalOpen(true);
+    }
+
+    setDraggedElement(null);
+  };
+
+  const handleDragStart = (e: React.DragEvent, id: string) => {
+    setDraggedElement(id);
+    e.dataTransfer.setData("text", "existingElement");
+    e.dataTransfer.setData("id", id);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (draggedElement) {
+      const { top, left } = e.currentTarget.getBoundingClientRect();
+      onMove(draggedElement, e.clientY - top, e.clientX - left);
+    }
+  };
+
+  const handleSave = () => {
+    if (editingElementId) {
+      // Update existing element
+      onEditMove(
+        editingElementId,
+        elementData.x,
+        elementData.y,
+        elementData.text,
+        elementData.fontSize,
+        elementData.fontWeight
+      );
+      setEditingElementId(null); // Reset editingElementId
+    } else if (currentElement) {
+      // Create new element
+      onDrop(
+        currentElement.type,
+        elementData.x,
+        elementData.y,
+        elementData.text,
+        elementData.fontSize,
+        elementData.fontWeight
+      );
+    }
+
+    setModalOpen(false);
+    setElementData({
+      text: "",
+      x: 0,
+      y: 0,
+      fontSize: 16,
+      fontWeight: "normal",
+    });
+  };
+
+  const renderElement = (element: {
+    id: string;
+    type: string;
+    top: number;
+    left: number;
+    text: string;
+    fontSize: number;
+    fontWeight: string;
+  }) => {
+    const style = {
+      fontSize: `${element.fontSize}px`,
+      fontWeight: element.fontWeight,
+      padding: "10px",
+    };
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+      if (e.key === "Enter") {
+        // Open the modal for the selected element
+        setEditingElementId(element.id);
+        setModalOpen(true);
+        setElementData({
+          text: element.text,
+          x: element.top,
+          y: element.left,
+          fontSize: element.fontSize,
+          fontWeight: element.fontWeight,
+        });
       }
     };
-    
-    return (
+
+    switch (element.type) {
+      case "input":
+        return (
+          <input
+            onKeyDown={handleKeyDown}
+            placeholder="helo"
+            value={element.text}
+            type="text"
+            style={style}
+          />
+        );
+      case "label":
+        return (
+          <div tabIndex={0} onKeyDown={handleKeyDown} style={style}>
+          {element.text}
+        </div>
+        );
+      case "button":
+        return (
+          <button onKeyDown={handleKeyDown} style={style}>
+            {element.text}
+          </button>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <>
       <div
         className="w-full relative h-full"
         onDrop={handleDrop}
@@ -54,13 +198,107 @@ const Board: React.FC<BoardProps> = ({ elements, onDrop, onMove }) => {
             draggable
             onDragStart={(e) => handleDragStart(e, element.id)}
           >
-            {element.top}, {element.left}
-            {element.type}
+            {renderElement(element)}
           </div>
         ))}
+        {modalOpen && (
+          <Dialog
+            sx={{
+              "& .MuiDialog-paper": {
+                width: ["100%", "75%", "25%"],
+                maxWidth: "none",
+              },
+            }}
+            open={modalOpen}
+            fullWidth
+          >
+            <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+              Edit {currentElement?.type}
+            </DialogTitle>
+            <DialogContent
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-evenly",
+                alignItems: "center",
+                gap: 5,
+              }}
+              dividers
+            >
+              <TextField
+                label="Text"
+                fullWidth
+                placeholder="hello"
+                type="text"
+                value={elementData.text}
+                onChange={(e) =>
+                  setElementData({ ...elementData, text: e.target.value })
+                }
+              />
+              <TextField
+                label="X"
+                fullWidth
+                type="number"
+                value={elementData.x}
+                onChange={(e) =>
+                  setElementData({
+                    ...elementData,
+                    x: parseInt(e.target.value),
+                  })
+                }
+              />
+              <TextField
+                label="Y"
+                fullWidth
+                type="number"
+                value={elementData.y}
+                onChange={(e) =>
+                  setElementData({
+                    ...elementData,
+                    y: parseInt(e.target.value),
+                  })
+                }
+              />
+              <TextField
+                label="Font size"
+                fullWidth
+                type="number"
+                value={elementData.fontSize}
+                onChange={(e) =>
+                  setElementData({
+                    ...elementData,
+                    fontSize: parseInt(e.target.value),
+                  })
+                }
+              />
+
+              <TextField
+                label="Font weight"
+                fullWidth
+                type="text"
+                value={elementData.fontWeight}
+                onChange={(e) =>
+                  setElementData({ ...elementData, fontWeight: e.target.value })
+                }
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => setModalOpen(false)}
+              >
+                Cancel
+              </Button>
+              <Button variant="contained" autoFocus onClick={handleSave}>
+                Save
+              </Button>
+            </DialogActions>
+          </Dialog>
+        )}
       </div>
-    );
-  };
-  
-  export default Board;
-  
+    </>
+  );
+};
+
+export default Board;
